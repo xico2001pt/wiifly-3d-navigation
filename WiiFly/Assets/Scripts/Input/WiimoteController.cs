5e97a0ec4b8e7f82a118802e0ff501d6301c4070
Francisco Cerqueira <up201905337@fe.up.pt>
2023-11-08 17:02:23 +0000

    feat: add interpolation speed

--- WiiFly/Assets/Scripts/Input/WiimoteController.cs
+++ WiiFly/Assets/Scripts/Input/WiimoteController.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 using WiiFly.Cursor;
 using WiimoteApi;
 
-namespace WiiFly.Input
-{
-    public class WiimoteController : MonoBehaviour
-    {
+namespace WiiFly.Input {
+    public class WiimoteController : MonoBehaviour {
         #region Fields
-        private Wiimote _wiimote;
         [SerializeField] private CursorController cursorController;
+        [SerializeField] private float interpolationSpeed = 5f;
+        
+        private Wiimote _wiimote;
+        private float _xPosition, _yPosition;
+        private float _targetXPosition, _targetYPosition;
         #endregion
 
-        private void Start()
-        {
+        #region Unity Methods
+        private void Start() {
             WiimoteManager.FindWiimotes();
             _wiimote = WiimoteManager.Wiimotes[0];
             _wiimote.SendDataReportMode(InputDataType.REPORT_INTERLEAVED);
@@ -22,36 +24,38 @@ namespace WiiFly.Input
             _wiimote.SetupIRCamera(IRDataType.FULL);
         }
 
-        private void OnDestroy()
-        {
-            if ( _wiimote != null )
-            {
+        private void OnDestroy() {
+            if ( _wiimote != null ) {
                 _wiimote.SendPlayerLED(false, true, false, false);
                 //WiimoteManager.Cleanup(_wiimote);
             }
         }
 
-        private void Update()
-        {
-            if ( _wiimote != null )
-            {
+        private void Update() {
+            if (_wiimote != null) {
                 int ret;
-                do
-                {
+                do {
                     ret = _wiimote.ReadWiimoteData();
-                    if (ret > 0)
-                    {
-                        var irp = _wiimote.Ir.GetIRMidpoint(true);
-                        Debug.Log(irp[0].ToString() + " " + irp[1].ToString());
-                        if (irp[0] > 0 && irp[1] > 0)
-                        {
-                            float x = Mathf.Clamp(-(irp[0] * 2 - 1), -1f, 1f);
-                            float y = Mathf.Clamp(irp[1] * 2 - 1, -1f, 1f);
-                            cursorController.SetCursorData(x, y);
-                        }
+                    if (ret > 0) {
+                        UpdateCursorPosition();
                     }
-                } while ( ret > 0 );
+                } while (ret > 0);
             }
         }
+        #endregion
+        
+        #region Private Methods
+        private void UpdateCursorPosition() {
+            var irp = _wiimote.Ir.GetIRMidpoint(true);
+            if (irp[0] > 0 && irp[1] > 0) {
+                _targetXPosition = Mathf.Clamp(-(irp[0] * 2 - 1), -1f, 1f);
+                _targetYPosition = Mathf.Clamp(irp[1] * 2 - 1, -1f, 1f);
+                
+                _xPosition = Mathf.Lerp(_xPosition, _targetXPosition, interpolationSpeed * Time.deltaTime);
+                _yPosition = Mathf.Lerp(_yPosition, _targetYPosition, interpolationSpeed * Time.deltaTime);
+                cursorController.SetCursorData(_xPosition, _yPosition);
+            }
+        }
+        #endregion
     }
 }
