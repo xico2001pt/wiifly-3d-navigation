d157a71c88d1cdb341ae265c2c34eb1fe8d2dc7a
Vasco52 <up201808031@g.uporto.pt>
2023-11-09 18:25:14 +0000

    improve mode widget

--- WiiFly/Assets/Scripts/GUI/ModeWidgetController.cs
+++ WiiFly/Assets/Scripts/GUI/ModeWidgetController.cs
@@ -2,23 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class ModeWidgetController : MonoBehaviour
-{
-    private Text textWidget; 
+namespace WiiFly.GUI {
+    public class ModeWidgetController : MonoBehaviour {
+        #region Fields
+        private TMP_Text _text;
+        private Image _image;
+        #endregion
 
-    private void Start()
-    {
-        textWidget = GetComponent<Text>();
-    }
+        #region Unity Methods
+        protected void Awake()
+        {
+            _text = GetComponentInChildren<TMP_Text>();
+            Image[] images = GetComponentsInChildren<Image>();
+            _image = images[1];
+        }
+        #endregion
 
-    public void ChangeToOrbitMode()
-    {
-        textWidget.text = "Orbit Mode";
-    }
+        #region Private Methods
+        private void ChangeToOrbitMode()
+        {
+            _text.text = "Orbit Mode";
+            _image.sprite = Resources.Load<Sprite>("Sprites/orbit");
+        }
 
-    public void ChangeToFlyMode()
-    {
-        textWidget.text = "Fly Mode";
+        private void ChangeToFlyMode()
+        {
+            _text.text = "Fly Mode";
+            _image.sprite = Resources.Load<Sprite>("Sprites/fly");
+        }
+        #endregion
     }
 }
